e10250e953073034a2a25532035ae3d5501df658
Federico Donati <federico.donati@kaleyra.com>
2022-11-18 23:49:48 +0100

    AHT10 temperature/rh probe: first release

---
+++ README.md
@@ -0,0 +1,11 @@
+#Temperature and relative humidity sensor
+Built used a Raspberry Pi Pico W and the AHT10 sensor.
+
+#AHT10
+This seems to be a very common low-cost sensor, but with a decent precision.
+I have compared the readings with other commercial sensors I have at home and the readins where very similar.
+
+Unfortunately the documentation is quite poor, and certain datasheets are terrible (bad formatting, with some parts written in Chinese), so it was difficult to understand how to read the values, and I helped myself looking at some C library available in other repositories.
+
+#Micropython
+The Raspberry Pi Pico W is a low-cost board with WiFi support which can be used with Micropython. All the ready-to-go libraries for the AHT10 I found around were using specific modules for the AHT10 or libraries like the smbus, which is not natively availabile in Micropython. Because I did not want to add any libray to the Pico, I wrote this simple module which is based on the I2C library. 
---
+++ aht10.py
@@ -0,0 +1,64 @@
+import utime
+import sys
+import machine
+
+#---- CONSTANTS --------------------------------------------------------------
+
+TP_ADDR = 0x38 
+TP_INIT = 0xE1
+TP_READ = 0xAC
+
+# float precision
+PRECISION = 2
+#-----------------------------------------------------------------------------
+
+# read from the I2C bus, stopping after the nbytes
+def bus_read(i2c, addr, nbytes=1):
+    if nbytes < 1:
+        return bytearray()   
+    data = i2c.readfrom(addr, nbytes)
+    return data
+
+# wirte on the I2C bus
+def bus_write(i2c, addr, data):
+    msg = bytearray()
+    msg.append(data)
+    i2c.writeto(addr, msg)
+
+def read_values(i2c):
+    # initialize the sensor
+    bus_write(i2c, TP_ADDR, TP_INIT)
+    # send measurement command
+    bus_write(i2c, TP_ADDR, TP_READ)
+    # sleep for 1 second - this should be shorter (75ms according to the
+    # datasheet), but on my board the sensor was returning a busy/reading
+    # state (MSB of the first response byte)
+    utime.sleep(1)
+    # read data from the sensor, stop after 7 ack/nbytes. See datasheet for reference
+    data = bus_read(i2c, TP_ADDR, 7)
+    return data
+
+def convert(i2c):
+    data = read_values(i2c)
+    # bt shifting of the binay data to rebuild the information
+    print(data[3], data[4], data[5])
+    bin_temp = ((data[3] & 0x0F) << 16) | (data[4] << 8) | data[5]
+    bin_rh= ((data[1] << 16) | (data[2] << 8) | data[3]) >> 4
+    # decimal conversion and rounding
+    temp = round((((bin_temp / 1048576) * 200 ) - 50), PRECISION)
+    rh   = round(((bin_rh / 1048576) * 100), PRECISION)
+    return temp, rh
+
+# setup the bus
+i2c = machine.I2C(0, scl=machine.Pin(1), sda=machine.Pin(0), freq=400000)
+
+# continuous reading
+while True:
+    reading=convert(i2c)
+    print('------------------------------------')
+    print(f'Temperature: {reading[0]} °C')
+    print(f'Relative Humidity: {reading[1]}%')
+    print('')
+    utime.sleep(2)
+
+sys.exit(0)
